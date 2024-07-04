6d253bf7cf50be4adee61e855c1226aec270aaae
Shelby <shelbygt500martin@gmail.com>
2024-07-04 16:16:14 -0400

    added name and info at top of files

--- action.js
+++ action.js
@@ -1,3 +1,6 @@
+// Author: Shelby Martin
+// File Name: about.html
+// Today's Date: 07/04/2024
 
 function action(){
     document.getElementsByClassName("form-unsubmitted")[0].classList.add("hidden")
