5e7d372ec8668084a87da5061f46632e0bdba4f6
Shelby <shelbygt500martin@gmail.com>
2024-07-04 15:43:49 -0400

    first upload, don't want to lose current progress

---
+++ action.js
@@ -0,0 +1,6 @@
+
+function action(){
+    document.getElementsByClassName("form-unsubmitted")[0].classList.add("hidden")
+    document.getElementsByClassName("form-submitted")[0].classList.remove("hidden")
+}
+
